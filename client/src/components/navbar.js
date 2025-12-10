494f8764772f7d54bc4afc666f31edece7fcf71c
sarthak <sarthakssingh49@gmail.com>
2025-12-11 00:07:17 +0530

    Completed Auth

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,25 +1,33 @@
 import { AppBar, Toolbar, Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setCookies("access_token", "", { path: "/" });
+    window.localStorage.removeItem("userID");
+    navigate("/auth");
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Button color="inherit" component={Link} to="/">
-          Home
-        </Button>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/create-recipe">Create</Button>
+        <Button color="inherit" component={Link} to="/saved-recipes">Saved</Button>
 
-        <Button color="inherit" component={Link} to="/create-recipe">
-          Create
-        </Button>
-
-        <Button color="inherit" component={Link} to="/saved-recipes">
-          Saved
-        </Button>
-
-        <Button color="inherit" component={Link} to="/auth">
-          Login/Register
-        </Button>
+        {!cookies.access_token ? (
+          <Button color="inherit" component={Link} to="/auth">
+            Login/Register
+          </Button>
+        ) : (
+          <Button color="inherit" onClick={logout}>
+            Logout
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   );
--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,32 +1,81 @@
 import React, { useState } from 'react'
+import axios from 'axios'
+import {useCookies} from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 const Auth = () => {
   return (
-    <div><Login /></div>
+    <div>
+      <Login />
+      <Register />
+    
+    </div>
   )
 }
 
 export default Auth
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [_, setCookies] = useCookies(["access_token"]);
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      const response = await axios.post("http://localhost:3001/auth/login", {
+      username, password
+    })
+    console.log(response);
+    setCookies("access_token", response.data.token);
+    window.localStorage.setItem("userID", response.data.userID);
+    navigate("/");
+    }
+
+    catch(err){
+      console.log(err)
+    }
+
+  }
+
+
   return (
-    <div>
-      Login from here
-    </div>
+    <Form username={username} setUsername={setUsername} password={password} setPassword={setPassword} label="Login" onSubmit={onSubmit} />
   )
 }
-//here
+
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const onSubmit = async  (event) => {
+    event.preventDefault();
+    const response = await axios.post("http://localhost:3001/auth/register", {
+      username, password
+    })
+
+    alert(response.data.message)
+   // console.log(response.data.message);
+  }
   return (
-    <div>
-      <form>
+    <Form username={username} setUsername={setUsername} password={password} setPassword={setPassword} label="Register" onSubmit={onSubmit} />
+  )
+}
+
+const Form = ({username, setUsername, password, setPassword, label, onSubmit}) => {
+  return (
+     <div>
+      <form onSubmit={onSubmit}>
+        <h2>{label}</h2>
         <label htmlFor="username">Username:</label>
         <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)}></input>
         <label htmlFor="password">Password:</label>
         <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)}></input>
+        <button type="submit">{label}</button>
       </form>
     </div>
+
   )
 }
