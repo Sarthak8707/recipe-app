55a3f985cb6f7395c83a7ab7ed53d4fe62b71312
sarthak <sarthakssingh49@gmail.com>
2025-12-15 01:07:28 +0530

    Fixed UI for Users

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -5,22 +5,39 @@ import {useCookies} from "react-cookie"
 const Home = () => {
   const [recipes, setRecipes] = useState([]);
   const [cookies, _] = useCookies(["access_token"]);
+  const [ae, setae] = useState(false);
 
-  useEffect(() => {
+   useEffect(() => {
     const fetchRecipes = async () => {
       const response = await axios.get("http://localhost:3001/recipes");
       console.log(response.data);
-      console.log(cookies.access_token);
+     // console.log(cookies.access_token);
       setRecipes(response.data)
     }
     fetchRecipes();
   }, [])
 
+  
+  if(ae){
+    return (
+      <div>
+        Sign in to save recipe
+      </div>
+    )
+  }
+
+ 
+
   const userID = window.localStorage.userID;
   
   const onSave = async (recipeID) => {
-    const response = await axios.put("http://localhost:3001/recipes", {userID, recipeID}, {headers: {authorization: cookies.access_token}});
-    console.log(response)
+    try{
+      const response =  await axios.put("http://localhost:3001/recipes", {userID, recipeID}, {headers: {authorization: cookies.access_token}});
+    }
+    catch (err){
+      setae(true)
+    }
+   // console.log(response)
   }
 
   return (
